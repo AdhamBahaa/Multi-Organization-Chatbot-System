b8c57260a26d7b924ca878cf3b9a7d5d382021ac
YH00013CU <133319846+YH00013CU@users.noreply.github.com>
2025-08-21 14:04:36 +0300

    database update

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# Database configuration
 DB_USER = os.getenv("DB_USER")    
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
